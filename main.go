52651a9305def6c86d98b77bc3b4d221d4ee7a90
Daniel322 <kravchenkodanil122@gmail.com>
2025-04-27 00:01:41 +0400

    add db connector

--- main.go
+++ main.go
@@ -1,7 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
+)
+
+var err error
+var conn *pgx.Conn
 
 func main() {
+	err = godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
 	fmt.Println("Hello!")
+	fmt.Println("try to connect to db")
+
+	conn, err = pgx.Connect(context.Background(), os.Getenv("GOOSE_DBSTRING"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer conn.Close(context.Background())
+
+	var rows pgx.Rows
+	var result []string
+
+	rows, err = conn.Query(context.Background(), "SELECT tablename FROM pg_catalog.pg_tables")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var startLoop = false
+	for !startLoop {
+		needNext := rows.Next()
+		if needNext {
+			var rowResult []any
+			rowResult, err = rows.Values()
+			if err != nil {
+				log.Fatal(err)
+			}
+			rowResultEl := rowResult[0]
+			result = append(result, rowResultEl.(string))
+			fmt.Println(rowResultEl)
+		} else {
+			startLoop = true
+		}
+	}
+
+	// result, err = rows.Values()
+
+	fmt.Println(result)
+
+	fmt.Println("Connected!")
 }
