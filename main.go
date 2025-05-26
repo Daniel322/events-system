c340051d7cad86cba3755c9abce861fa0229c56b
Daniel322 <kravchenkodanil122@gmail.com>
2025-05-26 23:52:43 +0300

    start to rewrite on GORM, rewrite user entity and service

--- main.go
+++ main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"context"
 	telegram_api "events-system/apis/telegram"
 	"events-system/modules/db"
 	"fmt"
-	"log"
 	"sync"
 
-	"github.com/jackc/pgx/v5"
 	"github.com/joho/godotenv"
 )
 
@@ -23,52 +20,8 @@ func main() {
 	if err != nil {
 		fmt.Println("Error loading .env file")
 	}
-	fmt.Println("Hello!")
-	fmt.Println("try to connect to db")
 
-	db.ConnectDatabase(context.Background())
-
-	defer db.Close(context.Background())
+	db.ConnectDatabase()
 
 	telegram_api.BootstrapBot()
-
-	var rows pgx.Rows
-	var result []string
-
-	if db.CheckConnection() {
-		fmt.Println("Connected!")
-	} else {
-		fmt.Println("Disconnected!")
-	}
-
-	rows, err = db.Connection.Query(context.Background(), "SELECT tablename FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema';")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var startLoop = false
-	for !startLoop {
-		needNext := rows.Next()
-		if needNext {
-			var rowResult []any
-			rowResult, err = rows.Values()
-			if err != nil {
-				log.Fatal(err)
-			}
-			rowResultEl := rowResult[0]
-			result = append(result, rowResultEl.(string))
-			fmt.Println(rowResultEl)
-		} else {
-			startLoop = true
-		}
-	}
-
-	fmt.Println(result)
-
-	// event_module.CreateEvent(event_module.CreateEventData{
-	// 	UserId:    "92e7e817-275a-4fe5-bf59-da72641c8549",
-	// 	Info:      "Moms birthdays",
-	// 	Date:      "1975-08-28T00:00:00.000Z",
-	// 	Providers: []string{"telegram"},
-	// }, context.Background())
 }
--- modules/db/db.service.go
+++ modules/db/db.service.go
@@ -1,64 +1,23 @@
 package db
 
 import (
-	"context"
-	"errors"
 	"fmt"
 	"log"
 	"os"
-	"reflect"
 
-	"github.com/jackc/pgx/v5"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 var err error
-var Connection *pgx.Conn
+var Connection *gorm.DB
 
-func ConnectDatabase(context context.Context) {
-	Connection, err = pgx.Connect(context, os.Getenv("GOOSE_DBSTRING"))
+func ConnectDatabase() {
+	Connection, err = gorm.Open(postgres.Open(os.Getenv("GOOSE_DBSTRING")))
+
 	if err != nil {
 		log.Fatal(err)
 	}
-}
 
-func CheckConnection() bool {
-	return !Connection.IsClosed()
-}
-
-func Close(context context.Context) {
-	Connection.Close(context)
-}
-
-func BaseQuery[T any](context context.Context, query string, args ...any) (*T, error) {
-	var result T
-	v := reflect.ValueOf(&result)
-	if v.Kind() != reflect.Ptr || (v.Elem().Kind() != reflect.Struct) {
-		fmt.Println("dest must be a pointer to struct")
-		return nil, errors.New("dest must be a pointer to struct")
-	}
-
-	v = v.Elem()
-	fields := make([]interface{}, v.NumField())
-
-	for i := 0; i < v.NumField(); i++ {
-		fields[i] = v.Field(i).Addr().Interface()
-	}
-
-	rowResult, err := Connection.Query(context, query, args...)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rowResult.Close()
-	if rowResult.Next() {
-		err = rowResult.Scan(
-			fields...,
-		)
-		if err != nil {
-			log.Fatal(err)
-		}
-	} else {
-		return nil, rowResult.Err()
-	}
-
-	return &result, nil
+	fmt.Println("Connected!")
 }
