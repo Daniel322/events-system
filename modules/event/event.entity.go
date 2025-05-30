649a781627f6cfd446ad07c0caa1e53fc1bae4de
Daniel322 <kravchenkodanil122@gmail.com>
2025-05-30 21:53:01 +0300

    fix event service

--- modules/event/event.entity.go
+++ modules/event/event.entity.go
@@ -11,7 +11,7 @@ type Event struct {
 	Info         string       `json:"info"`
 	Date         time.Time    `json:"date"`
 	NotifyLevels []string     `json:"notify_levels"`
-	Providers    []string     `json:"providers"`
+	Providers    []byte       `json:"providers"`
 	CreatedAt    sql.NullTime `json:"created_at"`
 	UpdatedAt    sql.NullTime `json:"updated_at"`
 }
