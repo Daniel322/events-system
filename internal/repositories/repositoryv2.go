5356f72c8291188e8e0c1c960619b7ab929279db
Daniel322 <kravchenkodanil122@gmail.com>
2025-08-16 13:51:56 +0300

    compelete new repo package

--- internal/repositories/repositoryv2.go
+++ internal/repositories/repositoryv2.go
@@ -13,18 +13,22 @@ func Init(conn *db.Database) {
 	connection = conn
 }
 
-// TODO: make private function for dynamic generate name for logs
+func getGenericName[Entity any](value Entity) string {
+	typeName := reflect.TypeOf(value).String()
+
+	return typeName + " repository:"
+}
 
 func checkTransactionExistance(transaction db.DatabaseInstance, name string) db.DatabaseInstance {
 	var instanceForExec db.DatabaseInstance
 
 	if reflect.ValueOf(transaction).Elem().IsValid() {
 		log.SetPrefix("INFO ")
-		log.Println(name + " repository: " + "transaction coming")
+		log.Println(name + "transaction coming")
 		instanceForExec = transaction
 	} else {
 		log.SetPrefix("INFO ")
-		log.Println(name + "repository: " + "transaction not exist")
+		log.Println(name + "transaction not exist")
 		instanceForExec = connection.Instance
 	}
 
@@ -32,7 +36,7 @@ func checkTransactionExistance(transaction db.DatabaseInstance, name string) db.
 }
 
 func Create[Entity any](data Entity, transaction db.DatabaseInstance) (*Entity, error) {
-	typeName := reflect.TypeOf(data).String()
+	typeName := getGenericName[Entity](data)
 	var instanceForExec = checkTransactionExistance(transaction, typeName)
 
 	result := instanceForExec.Create(&data)
@@ -46,12 +50,12 @@ func Create[Entity any](data Entity, transaction db.DatabaseInstance) (*Entity, 
 
 func GetById[Entity any](id string) (*Entity, error) {
 	entity := new(Entity)
-	typeName := reflect.TypeOf(entity).String()
+	typeName := getGenericName(entity)
 
 	result := connection.Instance.First(entity, "id =?", id)
 
 	if result.Error != nil {
-		return nil, utils.GenerateError(typeName+" repository", result.Error.Error())
+		return nil, utils.GenerateError(typeName, result.Error.Error())
 	}
 
 	return entity, nil
@@ -59,11 +63,11 @@ func GetById[Entity any](id string) (*Entity, error) {
 
 func Delete[Entity any](id string, transaction db.DatabaseInstance) (bool, error) {
 	entity := new(Entity)
-	typeName := reflect.TypeOf(entity).String()
+	typeName := getGenericName(entity)
 	parsedId, _, err := utils.ParseId(id)
 
 	if err != nil {
-		return false, utils.GenerateError(typeName+" repository", err.Error())
+		return false, utils.GenerateError(typeName, err.Error())
 	}
 
 	var instanceForExec = checkTransactionExistance(transaction, typeName)
@@ -71,8 +75,44 @@ func Delete[Entity any](id string, transaction db.DatabaseInstance) (bool, error
 	result := instanceForExec.Where("id = ?", parsedId).Delete(&entity)
 
 	if result.Error != nil {
-		return false, utils.GenerateError(typeName+" repository", result.Error.Error())
+		return false, utils.GenerateError(typeName, result.Error.Error())
 	}
 
 	return true, nil
+}
+
+func GetList[Entity any](options map[string]interface{}) (*[]Entity, error) {
+	var entities *[]Entity
+	typeName := getGenericName(entities)
+
+	result := connection.Instance.Where(options).Find(&entities)
+
+	if result.Error != nil {
+		return nil, utils.GenerateError(typeName, result.Error.Error())
+	}
+
+	return entities, nil
+}
+
+func Update[Entity any](
+	id string,
+	data Entity,
+	transaction db.DatabaseInstance,
+) (*Entity, error) {
+	entity, err := GetById[Entity](id)
+	typeName := getGenericName(entity)
+
+	if err != nil {
+		return nil, utils.GenerateError(typeName, err.Error())
+	}
+
+	var instanceForExec = checkTransactionExistance(transaction, typeName)
+
+	result := instanceForExec.Save(entity)
+
+	if result.Error != nil {
+		return nil, utils.GenerateError(typeName, result.Error.Error())
+	}
+
+	return entity, nil
 }
