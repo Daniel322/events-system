71a68ce1c26b2bb1a8d6897caa87bc62c484bcdb
Daniel322 <kravchenkodanil122@gmail.com>
2025-09-13 00:23:54 +0300

    work on tests for utils

--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -1,7 +1,33 @@
 package utils
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/google/uuid"
+)
 
 func TestParseId(t *testing.T) {
+	testUuid := uuid.New()
 
+	_, _, err := ParseId(testUuid)
+
+	if err != nil {
+		t.Error("ParseId should return uuid in string format, but return error")
+	}
+
+	invalidTestUuid := "invalidstring"
+
+	_, _, err = ParseId(invalidTestUuid)
+
+	if err == nil {
+		t.Error("ParseId should return error, but error was nil")
+	}
+}
+
+func TestGenerateError(t *testing.T) {
+	err := GenerateError("testName", "some error")
+
+	if err == nil {
+		t.Error("should be error, but get nil")
+	}
 }
